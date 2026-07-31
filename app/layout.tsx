50eee55c65c2b1ee5192a91add379788072bb545
unknown <wnsdud131700@gmail.com>
2026-07-31 18:31:06 +0900

    Remove duplicate 2026 in footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           {children}
         </main>
         <footer className="p-6 text-center text-xl text-gray-600">
-          © {new Date().getFullYear()} 2026-2 기하 부교재 해설강의. All rights reserved.
+          © 2026-2 기하 부교재 해설강의. All rights reserved.
         </footer>
       </body>
     </html>
